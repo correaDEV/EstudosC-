4f065363a8bdd85c1083bdca2676371858a5f55e
Eduardo correa <Eduzeraaoficial@gmail.com>
2025-08-26 23:35:43 -0300

    Adicionar arquivos de projeto.

---
+++ Pratica1/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace HelloWord
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string nome = "João";
+            int idade = 19;
+            float altura = 1.75f;
+            bool masculino = true;  
+
+            Console.WriteLine($"Nome: {nome}, Idade: {idade}, Altura: {altura}, Masculino: {masculino}");
+
+        }
+    }
+}
